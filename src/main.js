345453a29726070872b5fc09ffaa3036c9d3e079
Tee <admin@jquerytips.com>
2016-12-09 12:00:09 +0700

    fix issue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 import 'bulma/bulma.sass'
 import 'font-awesome/css/font-awesome.css'
 import App from './App'
 import store from './store'
-
-Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
